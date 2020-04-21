3497b0dca315ae896d49301f12d08143b2c12b02
nafari.n <nora.nafari@gmail.com>
2020-04-21 11:39:30 +0430

    Added emitter method

--- src/main/java/com/noran/demo/controller/Controller.java
+++ src/main/java/com/noran/demo/controller/Controller.java
@@ -6,17 +6,18 @@ import com.noran.demo.model.Employee;
 import com.noran.demo.repository.EmployeeRepository;
 import com.noran.demo.service.RandomStringGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.time.Duration;
-import java.time.LocalTime;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 @RestController
@@ -52,10 +53,32 @@ public class Controller {
     public Flux<ServerSentEvent<Stream<String>>> strings() {
         List<String> listOfStrings = stringGenerator.generate();
         return Flux.interval(Duration.ofSeconds(10))
-                .map(sequence -> ServerSentEvent.<Stream<String>> builder()
+                .map(sequence -> ServerSentEvent.<Stream<String>>builder()
                         .id(String.valueOf(sequence))
                         .data(Lists.partition(listOfStrings, 100).iterator().next().stream())
                         .build());
+    }
+
+    //using emitter
+    @GetMapping("/emitter")
+    public SseEmitter emit() throws IOException {
+        ExecutorService service = Executors.newCachedThreadPool();
+        List<String> strings = stringGenerator.generate();
+        SseEmitter emitter = new SseEmitter(1000000L);
+        service.execute(() ->
+        {
+            try {
+                for (int i = 0; i < 1000; ) {
+                    emitter.send(strings.subList(i, i + 100));
+                    i += 100;
+                    Thread.sleep(10000);
+                }
+                emitter.complete();
+            } catch (IOException | InterruptedException e) {
+                emitter.completeWithError(e);
+            }
+        });
+        return emitter;
     }
 
 }
